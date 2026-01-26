fb6ca6b13700bdd726437117a4edb84c77f5e9b5
guilhermeomrizzi123 <100003967+guilhermeomrizzi123@users.noreply.github.com>
2026-01-26 14:58:51 -0300

    feat: update version to 0.1.1 and version date to 2026-01-26

--- gridlamedit/__init__.py
+++ gridlamedit/__init__.py
@@ -3,8 +3,8 @@
 from .core.paths import package_path, resource_path, is_frozen
 
 APP_NAME = "GridLamEdit"
-__version__ = "0.1.0"
-__version_date__ = "2026-01-19"
+__version__ = "0.1.1"
+__version_date__ = "2026-01-26"
 __contact__ = "Guilherme Rizzi Contato +5512988508042"
 
 __all__ = [
